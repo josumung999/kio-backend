c1272cbe1eaca775b37d2012a3be9ff0aaac6602
Josue Munganga <39856434+josumung999@users.noreply.github.com>
2022-07-24 15:09:52 +0200

    Worked on the User Model

---
+++ models/User.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  phone: {
+    type: String,
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  firstname: {
+    type: String,
+    required: true
+  },
+  balance: {
+    type: Number,
+    default: 0
+  },
+  balanceOnHold: {
+    type: Number,
+    default: 0
+  },
+  role: {
+    type: String,
+    enum: ['client', 'admin'],
+    default: 'client'
+  }
+},
+{
+  timestamps: true
+}
+);
+
+module.exports = User = mongoose.model('user', UserSchema);
+
+
+
+
+
