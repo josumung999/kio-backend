b09b8faf651cdc1b64c097dc0c901a5a31946e6e
Josue Munganga <39856434+josumung999@users.noreply.github.com>
2022-07-26 09:36:53 +0200

    Securing the transactions route

--- routes/api/transactions.js
+++ routes/api/transactions.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+
+// Import auth middleware
+const auth = require('../../middleware/auth');
+
+// Import models
+const User = require('../../models/User');
+const Transaction = require('../../models/Transaction')
 
 
 
-// @route GET api/transactions
-// @desc Test route for transactions
-// @access PUBLIC
-router.get('/', (req, res) => {
+
+// @route POST api/transactions
+// @desc create a new transaction
+// @access PRIVATE
+router.get('/', auth, (req, res) => {
   res.send('Transactions Route')
 });
 
