5d6d6930952294d9e2a72b583392adbfb2764935
Josue Munganga <39856434+josumung999@users.noreply.github.com>
2022-07-26 09:58:48 +0200

    Built the data validation logic

--- routes/api/transactions.js
+++ routes/api/transactions.js
@@ -15,7 +15,23 @@ const Transaction = require('../../models/Transaction')
 // @route POST api/transactions
 // @desc create a new transaction
 // @access PRIVATE
-router.get('/', auth, (req, res) => {
+router.post('/', [
+  auth,
+  [
+    check('transactionType')
+      .isIn(['DEPOSIT', 'TRANSFER', 'PAYMENT', 'WITHDRAWAL', 'RECHARGE']).withMessage('Type de transaction invalide'),
+    check('amount', 'Veuillez spécifier le montant de la transaction').not().isEmpty(),
+    check('paymentMethod', 'Mode de paiement invalide')
+      .isIn(['AGENT', 'MOBILE MONEY', 'WAARI', 'BANK', 'CREDIT CARD', 'WORLD REMIT', 'SYSTEM']),
+    check('description', 'Description de la transaction requise').not().isEmpty()
+  ]
+],  async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
   res.send('Transactions Route')
 });
 
